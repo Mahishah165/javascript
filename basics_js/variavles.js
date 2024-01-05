4039d4a9be098232945e3cfdb7abcfea98b6e8e0
Mahishah165 <141489343+Mahishah165@users.noreply.github.com>
2024-01-05 16:40:09 +0000

    variables in js

---
+++ basics_js/variavles.js
@@ -0,0 +1,16 @@
+/* prefer not to use var for declaring variables in javascript
+const --> for declaring variables with constant value
+let -->to declare a variable whose value can be changed
+*/
+
+const id = 12344
+let nameS ="saara"
+var age="32"
+city = "pune"
+
+
+nameS = "mahi"
+age = "19"
+city = "mumbai"
+
+console.table([nameS,age,city])
